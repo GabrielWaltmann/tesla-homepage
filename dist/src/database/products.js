cc39ada65b51a1cd8f7aad0f1d3a6dbcdbd41c07
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2022-12-02 06:45:50 -0300

    Updated image quality

--- dist/src/database/products.js
+++ dist/src/database/products.js
@@ -1,44 +1,44 @@
 export const products = [
     {
         name: 'Model 3', 
-        url: 'Model3.jpg', 
+        url: 'Model3.png', 
         description: 'Schedule a Test Drive', 
         buttonOne: 'Custom Ordem', 
         buttonTwo: 'Existing Inventory'
     },
     {
         name: 'Model Y', 
-        url: 'ModelY.jpg' , 
+        url: 'ModelY.png' , 
         description: 'Schedule a Test Drive', 
         buttonOne: 'Custom Ordem', 
         buttonTwo: 'Existing Inventory'},
     {
         name: 'Model S', 
-        url: 'ModelS.jpg' , 
+        url: 'ModelS.png' , 
         description: 'Schedule a Test Drive', 
         buttonOne: 'Custom Ordem', 
         buttonTwo: 'Existing Inventory'},
     {
         name: 'Model X', 
-        url: 'ModelX.jpg' , 
+        url: 'ModelX.png' , 
         description: 'Schedule a Test Drive', 
         buttonOne: 'Custom Ordem', 
         buttonTwo: 'Existing Inventory'},
     {
         name: 'Solar Panels', 
-        url: 'SolarPanels.jpg' , 
+        url: 'SolarPanels.png' , 
         description: 'Lowest Cost Solar Panels in America', 
         buttonOne: 'Ordem Now', 
         buttonTwo: 'Learn More'},
     {
         name: 'Solar Roof', 
-        url: 'SolarRoof.jpg' , 
+        url: 'SolarRoof.png' , 
         description: 'Produce Clean Energy From Your Roof' , 
         buttonOne: 'Ordem Now', 
         buttonTwo: 'Learn More'},
     {
         name: 'Accessories', 
-        url: 'Accessories.jpg' , 
+        url: 'Accessories.png' , 
         description: '',
         buttonOne: 'Shop Now', 
         buttonTwo: false
